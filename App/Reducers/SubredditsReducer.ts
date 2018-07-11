a4bcf161aa74981f5b75316e152cd7d944652499
caili <li.cai@jfjun.com>
2018-07-11 16:14:35 +0800

    subredditsReducer: 修正tslint

--- App/Reducers/SubredditsReducer.ts
+++ App/Reducers/SubredditsReducer.ts
@@ -1,61 +1,61 @@
 import { handleActions } from "redux-actions";
-import { getSubredditPostsStartAction, getSubredditPostsSuccessAction, getSubredditPostsErrorAction,
-    GetSubredditPostsStartActionPayload, GetSubredditPostsSuccessActionPayload,
-    GetSubredditPostsErrorActionPayload } from "./../Actions/GetSubredditPostsActions";
+import {
+  getSubredditPostsStartAction, getSubredditPostsSuccessAction, getSubredditPostsErrorAction,
+  GetSubredditPostsStartActionPayload, GetSubredditPostsSuccessActionPayload,
+  GetSubredditPostsErrorActionPayload,
+} from "./../Actions/GetSubredditPostsActions";
 import { SubredditsState } from "./../Store/State/SubredditsState";
 
 const initialState: SubredditsState = {
-    items: {},
-    selectedSubreddit: undefined,
+  items: {},
+  selectedSubreddit: undefined,
 };
 
-export default handleActions<SubredditsState>({
-    [getSubredditPostsStartAction.toString()]: (state, action: ReduxActions.Action<GetSubredditPostsStartActionPayload>) => {
-        return {
-            selectedSubreddit: action.payload.subreddit,
-            items: {
-                ...state.items,
-                [action.payload.subreddit]: {
-                    name: action.payload.subreddit,
-                    isLoading: true,
-                    isError: false,
-                    posts: [],
-                },
-            },
-        };
-    },
+export default handleActions<SubredditsState, GetSubredditPostsErrorActionPayload & GetSubredditPostsSuccessActionPayload>({
+  [getSubredditPostsStartAction.toString()]: (state, action) => {
+    return {
+      selectedSubreddit: action.payload.subreddit,
+      items: {
+        ...state.items,
+        [action.payload.subreddit]: {
+          name: action.payload.subreddit,
+          isLoading: true,
+          isError: false,
+          posts: [],
+        },
+      },
+    };
+  },
+  [getSubredditPostsSuccessAction.toString()]: (state, action) => {
+    const updatedItem = {
+      ...state.items[action.payload.subreddit],
+      isLoading: false,
+      posts: action.payload.posts,
+    };
 
-    [getSubredditPostsSuccessAction.toString()]: (state, action: ReduxActions.Action<GetSubredditPostsSuccessActionPayload>) => {
-        const updatedItem = {
-            ...state.items[action.payload.subreddit],
-            isLoading: false,
-            posts: action.payload.posts,
-        };
+    return {
+      ...state,
+      items: {
+        ...state.items,
+        [action.payload.subreddit]: updatedItem,
+      },
+    };
+  },
 
-        return {
-            ...state,
-            items: {
-                ...state.items,
-                [action.payload.subreddit]: updatedItem,
-            },
-        };
-    },
+  [getSubredditPostsErrorAction.toString()]: (state, action) => {
+    const errorItem = {
+      ...state.items[action.payload.subreddit],
+      isLoading: false,
+      isError: true,
+      posts: [],
+    };
 
-    [getSubredditPostsErrorAction.toString()]: (state, action: GetSubredditPostsErrorActionPayload) => {
-        const errorItem = {
-            ...state.items[action.subreddit],
-            isLoading: false,
-            isError: true,
-            posts: [],
-        };
-
-        return {
-            ...state,
-            items: {
-                ...state.items,
-                [action.subreddit]: errorItem,
-            },
-        };
-    },
-
+    return {
+      ...state,
+      items: {
+        ...state.items,
+        [action.payload.subreddit]: errorItem,
+      },
+    };
+  },
 }, initialState);
